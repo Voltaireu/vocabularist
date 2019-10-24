859d0de81e6bcda4557f1bbc3f53cb37c948ba30
Voltaireu <rniziolek@hotmail.com>
2019-10-24 17:51:54 +0200

    Initial commit

---
+++ src/main/java/com/voltaireu/vocabularist/VocabularistApplication.java
@@ -0,0 +1,13 @@
+package com.voltaireu.vocabularist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voltaireu/vocabularist/VocabularistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voltaireu.vocabularist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
